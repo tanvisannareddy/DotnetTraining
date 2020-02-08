bd214ee9fdf418eba9bcbac2647fe632b3da9b0e
tanvisannareddy <60807451+tanvisannareddy@users.noreply.github.com>
2020-02-08 15:13:19 +0530

    Add files via upload

---
+++ bankcomp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bankcomponent
+{
+    public class Savingsbank
+    {
+        public int BalanceComponent;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+
+        public double Balance { get; set; }
+        public double GetSimpleInterest( double pr,int duration,float rate )
+
+        {
+            var si = (pr * duration * rate) / 100;
+            
+            return si;
+           
+        }
+
+        public  double GetBalance( double si)
+        {
+            double T = Balance + si;
+            return T;
+        }
+
+
+
+
+
+        }
+    }
+
