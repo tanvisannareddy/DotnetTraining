a7f02dd55af8245a5a073ca4cae743c3d52f698d
tanvisannareddy <60807451+tanvisannareddy@users.noreply.github.com>
2020-02-08 15:27:32 +0530

    Add files via upload

---
+++ CSharp/abstraction.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstractclassesdemo_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SBAccount objSB = new SBAccount();
+
+            CCAccount objCC = new CCAccount();
+
+            double siSB = objSB.GetSimpleInterest(95000.00, 5, 8);
+            double tamt = objSB.GetTotalAmount(50000.00, siSB);
+
+            Console.WriteLine("Simple Interest: Rs {0}", siSB);
+            Console.WriteLine("Total Amount: Rs {0}", tamt);
+
+            var siCC = objCC.GetSimpleInterest(95000.00, 5, 8);
+            tamt = objCC.GetTotalAmount(50000.00, siCC);
+
+            Console.WriteLine("Simple Interest(Current Account): Rs {0}", siCC);
+            Console.WriteLine("TotalAmount: Rs {0}", tamt);
+
+            objSB.Show(); //Calling the concrete method defined within the Banking base class
+
+        }
+    }
+    public abstract class Banking
+    {
+        public abstract double GetSimpleInterest(double pr, int id, short rt);
+
+        public abstract double GetTotalAmount(double bal, double si);
+
+        public void Show()
+        {
+            Console.WriteLine("This is a concrete method defined within an abstract class");
+        }
+    }
+
+    public class SBAccount : Banking
+    {
+        public override double GetSimpleInterest(double pr,int td,short rt)
+        {
+            double si = (pr * td * rt) / 100;
+            return si;
+        }
+
+        public override double GetTotalAmount(double bal, double si)
+        {
+            double tamount = bal + si;
+            return tamount;
+        }
+    }
+
+    public class CCAccount : Banking
+    {
+        public override double GetSimpleInterest(double pr, int td, short rt)
+        {
+            double si = 0.25 * ((pr * td * rt) / 100);//only 25% interest as that of SB Account
+            return si;
+        }
+
+        public override double GetTotalAmount(double bal,double si)
+        {
+            double tamount = bal + si;
+            return tamount;
+        }
+    }
+}
---
+++ CSharp/boxingandunboxing.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boxingandunboxing_programs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //boxing and unboxing
+
+            object obj = 100;//object is a reference type
+
+            int a = 100;
+
+            obj = a;
+
+            int b = (int)a;
+
+            Console.WriteLine("values of a and b = {0} and {1}", a, b);
+
+            //passing arguments by value type
+
+            int i, j;
+            i = 10; j = 20;
+
+            Console.WriteLine("values of i and j before function call = {0},{1}", i, j);
+            ShowData1(i, j);
+
+            Console.WriteLine("values of i and j after function call = {0},{1}", i, j);
+
+            //passing arguments by reference
+            int k, l;
+            k = 10; l = 20;
+            Console.WriteLine("values of k and l before function call = {0},{1}", k, l);
+            ShowData2(ref k, ref l);
+            Console.WriteLine("values of k and l after function call = {0},{1}", k, l);
+
+
+            Console.ReadLine();
+        }
+        public static void ShowData1(int a, int b)
+        {
+            double sum = a + b;
+            Console.WriteLine("sum = {0}", sum);
+            a += 5;
+            b += 5;
+        }
+        public static void ShowData2(ref int a, ref int b)
+        {
+            double sum = a + b;
+            Console.WriteLine("sum = {0}", sum);
+            a += 5;
+            b += 5;
+        }
+
+    }
+
+}
+
